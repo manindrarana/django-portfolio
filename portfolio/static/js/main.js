7c9bf62890ebe231e686ec1c15f859a2ae9f0600
manindrarana <ranamanindra9@gmail.com>
2024-01-15 20:32:12 +0000

    updated with static folder/bootstrap/font-awesome/

---
+++ portfolio/static/js/main.js
@@ -0,0 +1,61 @@
+//text typing
+
+const aboutText = document.getElementById("about-text");
+const textArray = [
+  "I am a student",
+  "I am studying the masters in informatics",
+];
+let index = 0;
+let isDeleting = false;
+let charIndex = 0;
+
+function typeText() {
+  const currentText = textArray[index];
+  if (isDeleting) {
+    aboutText.textContent = currentText.substring(0, charIndex - 1) + "_";
+    charIndex--;
+  } else {
+    aboutText.textContent = currentText.substring(0, charIndex + 1) + "_";
+    charIndex++;
+  }
+
+  if (charIndex <= 0) {
+    isDeleting = false;
+    index++;
+  }
+
+  if (charIndex === currentText.length + 1) {
+    isDeleting = true;
+    setTimeout(() => {
+      typeText();
+    }, 100);
+  } else {
+    setTimeout(typeText, 100);
+  }
+
+  if (index === textArray.length) {
+    index = 0;
+  }
+}
+
+typeText();
+
+//bottomto top
+const bottomToTopBtn = document.getElementById("bottomToTopBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    bottomToTopBtn.style.display = "block";
+  } else {
+    bottomToTopBtn.style.display = "none";
+  }
+}
+
+bottomToTopBtn.onclick = function () {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0;
+};
--- project/settings.py
+++ project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], #when template is moved in root dir 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
